126c5e61094db294099a45406424e27cffc90571
abhi05sep <ashish5991abhinav@gmail.com>
2026-03-27 16:32:10 +0530

    added some basic example

---
+++ week1_linux_basics/day1_basic_commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Day 1: Basic Linux Commands Practice
+
+echo "Current directory:"
+pwd
+
+echo "Listing files:"
+ls -l
+
+echo "Changing to home directory:"
+cd ~
+pwd
+
+echo "Manual page for ls (press q to quit):"
+man ls
