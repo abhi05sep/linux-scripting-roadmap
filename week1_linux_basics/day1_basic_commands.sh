e094d3084409c5e1ecdd92213d1be69fb6a48a6e
abhi05sep <159612083+abhi05sep@users.noreply.github.com>
2026-03-27 19:05:36 +0530

    Update day1_basic_commands.sh
    added some directory.

--- week1_linux_basics/day1_basic_commands.sh
+++ week1_linux_basics/day1_basic_commands.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Day 1: Basic Linux Commands Practice
-
+# Directories in Linux
+   # Root “/”: The base of the file system, containing all other directories .
+   # /bin and /sbin: Contains essential user and system executable files .
+   # /etc: Holds configuration files for system-wide settings .
+   # /dev: Represents devices as files, crucial for managing hardware .
+   # /tmp: Temporary storage, cleared upon reboot .
 echo "Current directory:"
 pwd
 
